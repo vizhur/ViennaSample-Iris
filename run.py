44b00fb44acf8db83c707833108d02095d67c888
Hai Ning <hning86@users.noreply.github.com>
2017-08-29 12:06:05 -0400

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 # set regularization rate as an argument 
 reg = 10
 while reg > 0.005:
-    os.system('az ml execute start -c local ./iris_sklearn.py {}'.format(reg))
+    os.system('az ml experiment submit -c local ./iris_sklearn.py {}'.format(reg))
     # cut regularization rate to half
     reg = reg / 2
     
