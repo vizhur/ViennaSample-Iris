d361ea82d9baf5916b8b96d905efefb27f4a79f8
Hai Ning <hning86@users.noreply.github.com>
2017-08-30 23:03:12 -0400

    Update iris_schema_gen.py

--- iris_schema_gen.py
+++ iris_schema_gen.py
@@ -1,13 +1,13 @@
 # This script generates the scoring and schema files
-# necessary to opearaitonalize the Iris Classification sample
-# Init and run functions
+# necessary to operationalize the Iris Classification sample
+#Init and run functions
 from azure.ml.api.schema.dataTypes import DataTypes
 from azure.ml.api.schema.sampleDefinition import SampleDefinition
-from azure.ml.api.realtime.services import prepare
+from azure.ml.api.realtime.services import generate_schema
 import pandas
 
 # Prepare the web service definition by authoring
-# init() and run() functions. Test the fucntions
+# init() and run() functions. Test the functions
 # before deploying the web service.
 def init():
     from sklearn.externals import joblib
@@ -18,6 +18,14 @@ def init():
 
 def run(input_df):
     import json
+    
+    # append 40 random features just like the training script does it.
+    import numpy as np
+    n = 40
+    random_state = np.random.RandomState(0)
+    n_samples, n_features = input_df.shape
+    input_df = np.c_[input_df, random_state.randn(n_samples, n)]
+
     pred = model.predict(input_df)
     return json.dumps(str(pred[0]))
 
@@ -31,5 +39,5 @@ run(input1)
 
 inputs = {"input_df": SampleDefinition(DataTypes.PANDAS, df)}
 # The prepare statement writes the scoring file (main.py) and
-# the scchema file (service_schema.json) the the output folder.
-prepare(run_func=run, init_func=init, input_types=inputs, )
+# the schema file (service_schema.json) the the output folder.
+generate_schema(run_func=run, inputs=inputs, filepath='service_schema.json')
